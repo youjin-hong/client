700f57d3d3cca88f6fda48f89ccf60eb7551109c
youjin-hong <asdfghjk1052@naver.com>
2025-05-04 16:26:00 +0900

    [ #17 | Chore ] 색상 theme 설정정

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -1,3 +1,36 @@
 export const colors = {
-  background: '#F5F5F5'
+  // 페이지 색상
+  background: '#F5F5F5',
+
+  // 텍스트 색상
+  text: {
+    dark: '#191919',
+    gray: '#8C8C8C',
+    white: '#FFFFFF'
+  },
+
+  // 기타 색상 (변수명이 좀 고민돼서 우선 진한순에서 연한순으로 숫자 넣어놓을게요..ㅎ)
+  // 청록 계열
+  teal_1: '#31A791',
+  teal_2: '#EFFEF9',
+
+  // 녹색 계열
+  green_1: '#4BA731',
+  green_2: '#F0FEED',
+
+  // 보라 계열
+  purple_1: '#A731A3',
+  purple_2: '#FFE7FE',
+
+  // 남색 계열
+  navy_1: '#3F31A7',
+  navy_2: '#EDEEFE',
+
+  // 갈색 계열
+  brown_1: '#A79131',
+  brown_2: '#FFF9E1',
+
+  // 빨강 계열
+  red_1: '#A73131',
+  red_2: '#FEEDED'
 };
