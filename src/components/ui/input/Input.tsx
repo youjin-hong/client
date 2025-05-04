2a9fe50e000a7a7287580b50c1d82750c001d849
youjin-hong <asdfghjk1052@naver.com>
2025-05-04 23:49:28 +0900

    [ #17 | Feat ] Input/label 컴포넌트 생성성

---
+++ src/components/ui/input/Input.tsx
@@ -0,0 +1,48 @@
+import { ChangeEvent, InputHTMLAttributes, useId } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  required?: boolean;
+  className?: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onEnterPress?: () => void;
+}
+
+export default function Input({
+  label,
+  value,
+  className = '',
+  required = false,
+  onChange,
+  onEnterPress,
+  ...props
+}: InputProps) {
+  const inputId = useId();
+
+  const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && onEnterPress) {
+      onEnterPress();
+    }
+  };
+
+  return (
+    <div>
+      {label && (
+        <label htmlFor={inputId} className="block mb-3 ml-2 font-bold text-14 text-typography-dark">
+          {label}
+          {required && <span> (*필수)</span>}
+        </label>
+      )}
+      <input
+        id={inputId}
+        type="text"
+        className={`w-full border border-typography-gray rounded-15 px-4 py-3 placeholder:text-typography-gray focus:border-typography-dark focus:outline-none ${className}`}
+        value={value}
+        onChange={onChange}
+        onKeyUp={handleKeyUp}
+        required={required}
+        {...props}
+      />
+    </div>
+  );
+}
