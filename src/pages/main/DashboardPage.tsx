ca24d7c54ef747280692c521aa7f9805882927c3
youjin-hong <asdfghjk1052@naver.com>
2025-05-05 00:06:50 +0900

    [ #17 | Fix ] 사이드바, 헤더 필요없는 페이지 조건 설정

--- src/pages/main/DashboardPage.tsx
+++ src/pages/main/DashboardPage.tsx
@@ -1,6 +1,3 @@
-import Button from '@/components/ui/button/Button';
-import Input from '@/components/ui/input/Input';
-
 export function DashboardPage() {
   return (
     <>
@@ -9,14 +6,6 @@ export function DashboardPage() {
         iusto, voluptas quia nemo repellat dolores ut iure, numquam amet labore quod inventore laboriosam sint iste
         tempora error.
       </h1>
-      <Button text="버튼" onClick={() => console.log('클릭')} />
-      <Input
-        label="이름"
-        placeholder="이름을 입력"
-        onChange={() => console.log('입력했다')}
-        onEnterPress={() => console.log('엔터')}
-        required
-      />
     </>
   );
 }
