6e92230b9220b2b4f5a4f1470b13c2594a4447e7
youjin-hong <asdfghjk1052@naver.com>
2025-05-08 08:01:23 +0900

    [ #22 | Feat ] 오늘 이후 날짜만 선택 가능한 util 함수 추가가

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -3,7 +3,7 @@ export const formatDate = (date: Date | string): string => {
   return d.toLocaleDateString('ko-KR', {
     year: 'numeric',
     month: 'long',
-    day: 'numeric',
+    day: 'numeric'
   });
 };
 
@@ -14,10 +14,16 @@ export const formatDateTime = (date: Date | string): string => {
     month: 'long',
     day: 'numeric',
     hour: '2-digit',
-    minute: '2-digit',
+    minute: '2-digit'
   });
 };
 
-export const formatNumber = (number: number): string => {
-  return new Intl.NumberFormat('ko-KR').format(number);
+// 오늘 이후로만 선택할 수 있게 날짜 설정하는 함수
+export const getFormattedToday = (): string => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
 };
