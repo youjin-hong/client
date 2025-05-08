a3a093551d4fe17db2f3ff7eef40f90bf9669d31
youjin-hong <asdfghjk1052@naver.com>
2025-05-08 10:33:36 +0900

    [ #22 | Refactor ] setting 프로필 수정의 전화번호 placeholder 추가

---
+++ src/pages/test/_components/TestTitle.tsx
@@ -0,0 +1,10 @@
+import TestIcon from '@/assets/icons/test.svg';
+
+export default function TestTitle() {
+  return (
+    <h1 className="flex gap-4 items-center pl-4 pb-10">
+      <img src={TestIcon} alt="project icon" />
+      <span className="font-bm text-20">테스트 관리</span>
+    </h1>
+  );
+}
