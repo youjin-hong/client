ddbc75b53d7f89156c68f03dff516e1aad50c607
youjin-hong <asdfghjk1052@naver.com>
2025-05-14 23:19:17 +0900

    [ #37 | Refactor ] 페이지 리스트 테이블 컴포넌트로 분리

--- src/pages/project/ProjectDetailPage.tsx
+++ src/pages/project/ProjectDetailPage.tsx
@@ -3,6 +3,7 @@ import ProjectTitle from '@/pages/project/_components/ProjectTitle';
 import ProjectInfo from '@/pages/project/_components/projectDetail/ProjectInfo';
 import ReportBrief from '@/pages/project/_components/projectDetail/ReportBrief';
 import ProjectControlButtons from '@/pages/project/_components/projectDetail/ProjectControlButtons';
+import ProjectPageTable from '@/pages/project/_components/projectDetail/ProjectPageTable';
 
 export default function ProjectDetailPage() {
   return (
@@ -12,46 +13,7 @@ export default function ProjectDetailPage() {
 
       <section className="flex gap-6 justify-center py-4 children:shadow-custom children:rounded-15 children:w-full">
         <div className="">그래프 섹션</div>
-        <div className="p-6">
-          <table className="w-full text-left">
-            <thead>
-              <tr className="text-14 text-typography-dark font-bold border-b border-b-[#CCCCCC]">
-                <th className="px-4 py-2">페이지명</th>
-                <th className="px-4 py-2">페이지 URL</th>
-                <th className="px-4 py-2">피그마 URL</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="children:font-medium text-11 text-typography-dark">
-                <td className="px-4 py-2">페이지이름</td>
-                <td className="px-4 py-2">
-                  <p>페이지유알엑</p>
-                </td>
-                <td className="px-4 py-2">
-                  <p>피그마유알엑</p>
-                </td>
-              </tr>
-              <tr className="children:font-medium text-11 text-typography-dark">
-                <td className="px-4 py-2">페이지이름</td>
-                <td className="px-4 py-2">
-                  <p>페이지유알엑</p>
-                </td>
-                <td className="px-4 py-2">
-                  <p>피그마유알엑</p>
-                </td>
-              </tr>
-              <tr className="children:font-medium text-11 text-typography-dark">
-                <td className="px-4 py-2">페이지이름</td>
-                <td className="px-4 py-2">
-                  <p>페이지유알엑</p>
-                </td>
-                <td className="px-4 py-2">
-                  <p>피그마유알엑</p>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <ProjectPageTable />
       </section>
 
       <ReportBrief />
---
+++ src/pages/project/_components/projectDetail/ProjectPageTable.tsx
@@ -0,0 +1,44 @@
+export default function ProjectPageTable() {
+  return (
+    <div className="p-6">
+      <table className="w-full text-left">
+        <thead>
+          <tr className="text-14 text-typography-dark font-bold border-b border-b-[#CCCCCC]">
+            <th className="px-4 py-2">페이지명</th>
+            <th className="px-4 py-2">페이지 URL</th>
+            <th className="px-4 py-2">피그마 URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="children:font-medium text-11 text-typography-dark">
+            <td className="px-4 py-2">페이지이름</td>
+            <td className="px-4 py-2">
+              <p>페이지유알엑</p>
+            </td>
+            <td className="px-4 py-2">
+              <p>피그마유알엑</p>
+            </td>
+          </tr>
+          <tr className="children:font-medium text-11 text-typography-dark">
+            <td className="px-4 py-2">페이지이름</td>
+            <td className="px-4 py-2">
+              <p>페이지유알엑</p>
+            </td>
+            <td className="px-4 py-2">
+              <p>피그마유알엑</p>
+            </td>
+          </tr>
+          <tr className="children:font-medium text-11 text-typography-dark">
+            <td className="px-4 py-2">페이지이름</td>
+            <td className="px-4 py-2">
+              <p>페이지유알엑</p>
+            </td>
+            <td className="px-4 py-2">
+              <p>피그마유알엑</p>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
