fa7c32e48d4f5012d2fe0b01830f52bce094f277
youjin-hong <asdfghjk1052@naver.com>
2025-05-26 10:33:36 +0000

    Update deploy.yml

--- src/pages/main/_components/DashboardTestTable.tsx
+++ src/pages/main/_components/DashboardTestTable.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import TableItem from '@/components/ui/table/CustomTable';
+import { DashBoardTestList } from '@/types/test.type';
 
 const columns = [
-  { id: 'name', label: '프로젝트 명' },
-  { id: 'type', label: '테스트 유형' },
-  { id: 'result', label: '성공 여부' },
-  { id: 'page', label: '페이지 명' }
+  { id: 'projectName', label: '프로젝트 명' },
+  { id: 'testType', label: '테스트 유형' },
+  { id: 'testStatus', label: '성공 여부' },
+  { id: 'pageName', label: '페이지 명' }
 ];
 
 const typeColor: Record<string, string> = {
@@ -14,23 +15,25 @@ const typeColor: Record<string, string> = {
   Component: 'text-yellow-600'
 };
 
-const DashboardTestTable: React.FC<{ tests: any[] }> = ({ tests }) => (
+const DashboardTestTable: React.FC<{ tests: DashBoardTestList[] }> = ({ tests }) => (
   <TableItem
     columns={columns}
     items={tests}
     renderCell={(column, item) => {
-      if (column.id === 'type') {
-        return <span className={`font-bold ${typeColor[item.type as keyof typeof typeColor]}`}>{item.type}</span>;
+      if (column.id === 'testStatus') {
+        return (
+          <span className={`font-bold ${typeColor[item.testType as keyof typeof typeColor]}`}>{item.testType}</span>
+        );
       }
-      if (column.id === 'result') {
+      if (column.id === 'testStatus') {
         return (
           <span
-            className={`px-3 py-1 rounded-full text-xs font-bold ${item.result === '성공' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}>
-            {item.result}
+            className={`px-3 py-1 rounded-full text-xs font-bold ${item.testStatus === '성공' ? 'bg-green-100 text-green-700' : 'bg-red-100 text-red-700'}`}>
+            {item.testStatus}
           </span>
         );
       }
-      return item[column.id] ?? '-';
+      return item[column.id as keyof DashBoardTestList] ?? '-';
     }}
   />
 );
