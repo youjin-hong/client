a9044e218a5d881be4eb014044af590aef8adf80
youjin-hong <asdfghjk1052@naver.com>
2025-05-28 17:18:56 +0900

    [ #49 | Design ] (중간 커밋) Title 컴포넌트 import

--- src/pages/test/TestDetailPage.tsx
+++ src/pages/test/TestDetailPage.tsx
@@ -1,3 +1,10 @@
+import TestTitle from '@/pages/test/_components/TestTitle';
+
 export default function TestDetailPage() {
-  return <div>테스트상세페이지</div>;
+  return (
+    <div className="w-[90%]  m-auto">
+      <TestTitle />
+      <hr className="w-full border-t-[1.5px] border-typography-gray my-2" />
+    </div>
+  );
 }
