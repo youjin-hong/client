de744d1393047a68f3ec65aaa0777379677da15c
youjin-hong <asdfghjk1052@naver.com>
2025-05-29 21:24:54 +0900

    [ #49 | Design ] 기본 원 size 150px로 수정정

---
+++ src/pages/test/_components/testDetail/TestRatingBars.tsx
@@ -0,0 +1,58 @@
+import CircleProgressBar from '@/components/ui/progressBar/CircleProgressBar';
+import { colors } from '@/styles/theme/colors';
+import { calculatePercentage } from '@/utils/useCalculateTestPercentage';
+
+interface TestRatingBarsProps {
+  testSummary: {
+    totalSuccessTests: number;
+    totalFailTests: number;
+    routingSuccessCount: number;
+    routingFailCount: number;
+    interactionSuccessCount: number;
+    interactionFailCount: number;
+    mappingSuccessCount: number;
+    mappingFailCount: number;
+  };
+}
+
+export default function TestRatingBars({ testSummary }: TestRatingBarsProps) {
+  const {
+    totalSuccessTests,
+    totalFailTests,
+    routingSuccessCount,
+    routingFailCount,
+    interactionSuccessCount,
+    interactionFailCount,
+    mappingSuccessCount,
+    mappingFailCount
+  } = testSummary;
+
+  return (
+    <section className="flex  gap-6 children:py-6">
+      <div className="shadow-custom rounded-15 basis-1/4 w-full px-2 children:w-full">
+        <CircleProgressBar
+          value={calculatePercentage(totalSuccessTests, totalSuccessTests + totalFailTests)}
+          label="TOTAL"
+          color="#E48989"
+        />
+      </div>
+      <div className="flex justify-around items-center shadow-custom rounded-15 basis-3/4 w-full px-2 children:w-full">
+        <CircleProgressBar
+          value={calculatePercentage(routingSuccessCount, routingSuccessCount + routingFailCount)}
+          label="ROUTING"
+          color={colors.teal_1}
+        />
+        <CircleProgressBar
+          value={calculatePercentage(interactionSuccessCount, interactionSuccessCount + interactionFailCount)}
+          label="INTERACTION"
+          color={colors.purple_1}
+        />
+        <CircleProgressBar
+          value={calculatePercentage(mappingSuccessCount, mappingSuccessCount + mappingFailCount)}
+          label="COMPONENT"
+          color={colors.brown_1}
+        />
+      </div>
+    </section>
+  );
+}
