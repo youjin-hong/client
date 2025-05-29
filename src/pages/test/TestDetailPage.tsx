a2c3778e1d9606f27f44a43b0c65803adec826f3
youjin-hong <asdfghjk1052@naver.com>
2025-05-29 21:26:34 +0900

    [ #49 | Feat ] 테스트 성공률 bar 섹션 개발 완료

--- src/pages/test/TestDetailPage.tsx
+++ src/pages/test/TestDetailPage.tsx
@@ -1,10 +1,37 @@
+import { useParams } from 'react-router-dom';
+import { useGetTestDetail } from '@/store/queries/test/useTestQueries';
+import ProjectInfo from '@/pages/project/_components/projectDetail/ProjectInfo';
 import TestTitle from '@/pages/test/_components/TestTitle';
+import TestRatingBars from '@/pages/test/_components/testDetail/TestRatingBars';
 
 export default function TestDetailPage() {
+  const params = useParams();
+  const projectId = params.projectId;
+  const { data: testDetail, isPending, isError } = useGetTestDetail(Number(projectId));
+
+  console.log('ddd', testDetail);
+
+  if (isPending) return <div className="py-20 text-center">로딩 중...</div>;
+  if (isError) return <div className="py-20 text-center text-red-500">오류가 발생했습니다.</div>;
+
+  const testBasicInfo = {
+    projectName: testDetail?.projectName,
+    projectCreatedDate: testDetail?.projectCreatedDate,
+    projectEnd: testDetail?.projectEnd,
+    projectAdmin: testDetail?.projectAdmin,
+    description: testDetail?.description,
+    testExecutionTime: testDetail?.testExecutionTime
+  };
+
   return (
     <div className="w-[90%]  m-auto">
       <TestTitle />
       <hr className="w-full border-t-[1.5px] border-typography-gray my-2" />
+      <div className="px-3 pt-2 pb-11">
+        <ProjectInfo {...testBasicInfo} />
+      </div>
+
+      <TestRatingBars testSummary={testDetail.testSummary} />
     </div>
   );
 }
