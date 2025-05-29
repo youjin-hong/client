8c5a2eceeb8b002d510db36ac2bf51abd2b21319
youjin-hong <asdfghjk1052@naver.com>
2025-05-29 20:57:23 +0900

    [ #49 | Feat ] 원형 progress bar 컴포넌트 생성

---
+++ src/components/ui/progressBar/CircleProgressBar.tsx
@@ -0,0 +1,58 @@
+import { CircularProgress, Box } from '@mui/material';
+
+interface CircleProgressBarProps {
+  value: number;
+  label?: string;
+  color?: string;
+  size?: number;
+  thickness?: number;
+}
+export default function CircleProgressBar({
+  value,
+  label = 'TOTAL',
+  color = '#E48989',
+  size = 143,
+  thickness = 5
+}: CircleProgressBarProps) {
+  return (
+    <div className="flex flex-col justify-center items-center gap-3">
+      <Box position="relative" display="inline-flex">
+        {/* 배경 회색원 */}
+        <CircularProgress
+          variant="determinate"
+          value={100}
+          size={size}
+          thickness={thickness}
+          sx={{
+            color: '#DDDDDD'
+          }}
+        />
+        {/* 진행률 원 */}
+        <CircularProgress
+          variant="determinate"
+          value={value}
+          size={size}
+          thickness={thickness}
+          sx={{
+            position: 'absolute',
+            left: 0,
+            color: color
+          }}
+        />
+      </Box>
+      {/* 테스트 이름, 진행률 text */}
+      <Box
+        top={0}
+        left={0}
+        bottom={0}
+        right={0}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center">
+        <p className="font-bold text-14 text-typography-dark">{label}</p>
+        <p className="font-bold text-14 text-typography-gray">{`${Math.round(value)}%`}</p>
+      </Box>
+    </div>
+  );
+}
