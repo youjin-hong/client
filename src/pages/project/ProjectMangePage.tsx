4c95b5d5a3ee58cda7fc4dfe2795dab68d25aee9
youjin-hong <asdfghjk1052@naver.com>
2025-06-06 01:32:16 +0900

    [ #60 | Design ] 임시로 팀원 column에 tailwind에서 제공해주는 avatar html 사용

--- src/pages/project/ProjectMangePage.tsx
+++ src/pages/project/ProjectMangePage.tsx
@@ -55,6 +55,32 @@ export default function ProjectMangePage() {
             if (column.id === 'projectStatus') {
               return <StatusBadge status={item.projectStatus} />;
             }
+            if (column.id === 'projectMember') {
+              return (
+                <div className="flex -space-x-1 overflow-hidden">
+                  <img
+                    className="inline-block size-6 rounded-full ring-2 ring-white"
+                    src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                  <img
+                    className="inline-block size-6 rounded-full ring-2 ring-white"
+                    src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                  <img
+                    className="inline-block size-6 rounded-full ring-2 ring-white"
+                    src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"
+                    alt=""
+                  />
+                  <img
+                    className="inline-block size-6 rounded-full ring-2 ring-white"
+                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    alt=""
+                  />
+                </div>
+              );
+            }
             if (column.id === 'testRate') {
               return <span>{item.testRate}%</span>;
             }
