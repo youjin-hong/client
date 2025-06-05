f8b73968cfce6602ccd696714cdde044a1c7f6fd
youjin-hong <asdfghjk1052@naver.com>
2025-06-05 23:48:14 +0900

    [ #49 | Feat ] test.type.ts에 페이지 api 타입 추가

--- src/types/test.type.ts
+++ src/types/test.type.ts
@@ -17,3 +17,85 @@ export interface DashBoardTestList {
   testType: string;
   testStatus: string;
 }
+
+export interface TestDetail {
+  projectName: string;
+  projectAdmin: string;
+  projectStart: string;
+  projectEnd: string;
+  testExecutionTime: string;
+  description: string;
+  testSummary: TestSummary;
+  pages: PageArrayProps[];
+}
+
+export interface TestSummary {
+  totalSuccessTests: number;
+  totalFailTests: number;
+  interactionSuccessCount: number;
+  interactionFailCount: number;
+  mappingSuccessCount: number;
+  mappingFailCount: number;
+  routingSuccessCount: number;
+  routingFailCount: number;
+}
+
+export interface PageArrayProps {
+  pageId: number;
+  pageName: string;
+}
+
+export interface RoutingTest {
+  success: RoutingSuccess[];
+  fail: RoutingFail[];
+}
+
+export interface RoutingSuccess {
+  triggerSelector: string;
+  expectedDestination: string;
+  actualDestination: string;
+}
+export interface RoutingFail {
+  triggerSelector: string;
+  expectedDestination: string;
+  actualDestination: string;
+  failReason: string;
+}
+
+export interface MappingTest {
+  matchedComponents: number;
+  componentNames: ComponentName[];
+  failComponents: FailComponent[];
+}
+
+export interface ComponentName {
+  componentName: string;
+}
+
+export interface FailComponent {
+  componentName: string;
+  failReason: string;
+}
+
+export interface InteractionTest {
+  success: InteractionSuccess[];
+  fail: InteractionFail[];
+}
+
+export interface InteractionSuccess {
+  trigger: string;
+  actualAction: string;
+}
+export interface InteractionFail {
+  trigger: string;
+  expectedAction: string;
+  actualAction: string;
+  failReason: string;
+}
+
+// useGetPageIssue 훅이 반환하는 data.data 에 해당하는 타입
+export interface PageIssue {
+  routingTest: RoutingTest;
+  mappingTest: MappingTest;
+  interactionTest: InteractionTest;
+}
