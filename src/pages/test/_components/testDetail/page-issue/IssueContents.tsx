647e8be367ec1afaedaa1050dc2d7be21f1d2946
youjin-hong <asdfghjk1052@naver.com>
2025-06-05 23:31:40 +0900

    [ #49 | Feat ] 이슈 콘텐츠 내용 구현현

---
+++ src/pages/test/_components/testDetail/page-issue/IssueContents.tsx
@@ -0,0 +1,81 @@
+import type { PageIssue } from '@/types/test.type';
+import type { TabMeta } from './IssueTabBar';
+
+interface IssueContentsProps {
+  issueData: PageIssue;
+  tabMeta: TabMeta;
+}
+
+export default function IssueContents({ issueData, tabMeta }: IssueContentsProps) {
+  const { category, index } = tabMeta;
+
+  let failItem:
+    | { expectedDestination: string; actualDestination: string; failReason: string }
+    | { componentName: string; failReason: string }
+    | { expectedAction: string; actualAction: string; failReason: string };
+
+  if (category === 'routing') {
+    failItem = issueData.routingTest.fail[index];
+  } else if (category === 'mapping') {
+    failItem = issueData.mappingTest.failComponents[index];
+  } else {
+    failItem = issueData.interactionTest.fail[index];
+  }
+
+  return (
+    <div className="pt-6 pb-2 space-y-8">
+      <div className="border-[1.5px] border-[#FF0000] rounded-[15px] bg-[#FF0000]/10 py-[22px] px-6 space-y-4">
+        <p className="font-bold text-14 text-typography-dark">{(failItem as { failReason: string }).failReason}</p>
+        {category === 'routing' && (
+          <div className="space-y-2">
+            <p className="font-medium text-11 text-typography-dark">
+              <span>기대 페이지:</span> {(failItem as { expectedDestination: string }).expectedDestination}
+            </p>
+            <p className="font-medium text-11 text-typography-dark">
+              <span>실제 페이지:</span> {(failItem as { actualDestination: string }).actualDestination}
+            </p>
+            <p className="font-medium text-11 text-typography-dark">
+              <span>실패 이유:</span> {(failItem as { failReason: string }).failReason}
+            </p>
+          </div>
+        )}
+
+        {category === 'mapping' && (
+          <div className="space-y-2">
+            <p className="font-medium text-11 text-typography-dark">
+              <span>컴포넌트 이름:</span> {(failItem as { componentName: string }).componentName}
+            </p>
+            <p className="font-medium text-11 text-typography-dark">
+              <span>실패 이유:</span> {(failItem as { failReason: string }).failReason}
+            </p>
+          </div>
+        )}
+
+        {category === 'interaction' && (
+          <div className="space-y-2">
+            <p className="font-medium text-11 text-typography-dark">
+              <span>기대 동작:</span> {(failItem as { expectedAction: string }).expectedAction}
+            </p>
+            <p className="font-medium text-11 text-typography-dark">
+              <span>실제 동작:</span> {(failItem as { actualAction: string }).actualAction}
+            </p>
+            <p className="font-medium text-11 text-typography-dark">
+              <span>실패 이유:</span> {(failItem as { failReason: string }).failReason}
+            </p>
+          </div>
+        )}
+      </div>
+
+      {/* TODO: LLM 개발 완료되면 수정하기 */}
+      <div className="border-[1.5px] border-[#0000FF] rounded-[15px] bg-[#0000FF]/10 py-[22px] px-6 space-y-2">
+        <p className="font-bold text-14 text-typography-dark">해결 제안</p>
+        <p className="font-medium text-11 text-typography-dark">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae animi, in esse aliquam tenetur ducimus
+          delectus nemo sapiente quos, dolor ea asperiores! Fugiat velit ipsam distinctio id dicta repellendus beatae.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae animi, in esse aliquam tenetur ducimus
+          delectus nemo sapiente quos, dolor ea asperiores! Fugiat velit ipsam distinctio id dicta repellendus beatae.
+        </p>
+      </div>
+    </div>
+  );
+}
