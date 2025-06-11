27b8a86fc01f4086e384baf677de392a2319b23a
youjin-hong <asdfghjk1052@naver.com>
2025-06-11 23:47:25 +0900

    [ #69 | Feat ] 프로젝트, 세부 페이지 테스트 재실행 버튼 추가

--- src/pages/project/_components/projectDetail/ProjectControlButtons.tsx
+++ src/pages/project/_components/projectDetail/ProjectControlButtons.tsx
@@ -5,6 +5,7 @@ import { useDeleteProject } from '@/store/queries/project/useProjectMutations';
 import CommonModal from '@/components/modal/CommonModal';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '@/constants';
+import { useRunTest } from '@/store/queries/test/useTestMutations';
 
 interface ProjectControlButtonProps {
   projectId: number;
@@ -14,6 +15,11 @@ export default function ProjectControlButtons({ projectId, projectName }: Projec
   const navigate = useNavigate();
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const { mutate: deleteProject } = useDeleteProject();
+  const { mutate: runTestMutation, isPending: isRunningTest, isError: isErrorRunningTest } = useRunTest();
+
+  const handleRunTest = () => {
+    runTestMutation(projectId);
+  };
 
   const handleEditProjectButtonClick = () => {
     navigate(ROUTES.EDIT_PROJECT.replace(':projectId', String(projectId)));
@@ -32,20 +38,31 @@ export default function ProjectControlButtons({ projectId, projectName }: Projec
   const handleCloseDeleteModal = () => {
     setIsDeleteModalOpen(false);
   };
+
+  // TODO: 테스트가 실행중이라는 것을 표현할 다른 방법으로 나중에 대체
+  if (isRunningTest) return <div className="py-20 text-center">테스트 실행 중...</div>;
+  if (isErrorRunningTest) return <div className="py-20 text-center text-red-500">테스트 중 오류가 발생했습니다.</div>;
   return (
     <>
       <section className="flex justify-end gap-3 pt-7">
         <Button
-          text="수정하기"
+          text="프로젝트 수정"
           leftIcon={<PencilIcon />}
           className="flex border-none shadow-custom rounded-10 w-[118px] h-6 items-center justify-center"
           onClick={handleEditProjectButtonClick}
         />
-        <Button
-          text="삭제하기"
-          className="border-none shadow-custom rounded-10 w-[118px] h-6 flex items-center justify-center"
-          onClick={handleProjectDeleteButtonClick}
-        />
+        <div>
+          <Button
+            text={isRunningTest ? '테스트 중' : '테스트 재실행'}
+            onClick={handleRunTest}
+            className="flex border-none shadow-custom rounded-10 w-[118px] h-6 items-center justify-center"
+          />
+          <button
+            className="block mx-auto my-2 border-0 border-b border-[#696969]  text-[#696969] font-medium text-11"
+            onClick={handleProjectDeleteButtonClick}>
+            프로젝트 삭제하기
+          </button>
+        </div>
       </section>
 
       <CommonModal
