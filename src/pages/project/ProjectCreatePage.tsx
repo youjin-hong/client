a6967bdd30bc5b1f8a7d77212893583463161fa7
youjin-hong <asdfghjk1052@naver.com>
2025-06-16 00:40:54 +0900

    [ #70 | Refactor ] 프로젝트 생성 페이지 커스텀 훅 호출

--- src/pages/project/ProjectCreatePage.tsx
+++ src/pages/project/ProjectCreatePage.tsx
@@ -1,39 +1,20 @@
-import { ROUTES } from '@/constants';
 import ProjectCreateForm from '@/pages/project/_components/projectForm/ProjectCreateForm';
-import { useProjectForm } from '@/pages/project/_hooks/useCreateProject';
-import { useUserProfile } from '@/store/queries/user/useUserQueries';
-import { useNavigate } from 'react-router-dom';
+import { useProjectFromHandler } from '@/pages/project/_hooks/useProjectFromHandler';
 
 export default function ProjectCreatePage() {
-  const navigate = useNavigate();
-  const { data: userData, isPending: isUserDataLoading, isError: isUserDataError } = useUserProfile();
+  const { isPending, isError, username, handleProjectSubmit, handleCancelProject, isLoading } = useProjectFromHandler({
+    mode: 'create'
+  });
 
-  const handleNavigate = (actionType: 'register' | 'test', projectId?: number) => {
-    if (actionType === 'register') {
-      navigate(ROUTES.PROJECT_DETAIL.replace(':projectId', String(projectId)));
-    } else if (actionType === 'test') {
-      navigate(ROUTES.TESTS);
-    }
-  };
-
-  const { handleProjectSubmit, isLoading } = useProjectForm(userData, handleNavigate);
-
-  const handleCancelProject = () => {
-    const isConfirmed = confirm('프로젝트 생성을 취소하시겠습니까?');
-    if (isConfirmed) {
-      navigate(ROUTES.HOME); // TODO: confirm 부분 나중에 modal로 바꾸기
-    }
-  };
-
-  if (isUserDataLoading) return <div className="py-20 text-center">사용자 정보 로딩 중...</div>;
-  if (isUserDataError || !userData)
+  if (isPending) return <div className="py-20 text-center">사용자 정보 로딩 중...</div>;
+  if (isError || !username)
     return <div className="py-20 text-center text-red-500">사용자 정보 불러오는 중 오류가 발생했습니다.</div>;
 
   return (
     <div className="w-[90%] m-auto">
       <h1 className="font-bm text-16 text-typography-dark pl-4 pb-8">새 프로젝트 생성</h1>
       <ProjectCreateForm
-        username={userData.username}
+        username={username}
         onSubmit={handleProjectSubmit}
         onCancel={handleCancelProject}
         isLoading={isLoading}
